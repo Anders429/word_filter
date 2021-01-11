bf8a2166c32da2eda89657b435da8c874d113953
Anders429 <anders.stefan.evensen@gmail.com>
2021-01-11 09:03:28 -0800

    Add documentation to pointer.

--- src/pointer.rs
+++ src/pointer.rs
@@ -1,24 +1,47 @@
 use crate::node::{Node, NodeType};
 
+/// The current status of the `Pointer`.
+///
+/// This indicates whether the `Pointer` has reached a `Match` or an `Exception` node.
 #[derive(Clone, Debug, PartialEq)]
 pub enum PointerStatus<'a> {
+    /// Indicates the `Pointer` has found no `Match` or `Exception` nodes yet.
     None,
+    /// Indicates the `Pointer` has found a `Match` node containing the stored string.
     Match(&'a str),
+    /// Indicates the `Pointer` has found an `Exception` node containing the stored string.
     Exception(&'a str),
 }
 
+/// A specialized pointer for traveling through the `WordFilter`'s `Node` graph.
+///
+/// The `Pointer` keeps track of the current context within the `Node` (as in, the current pointer
+/// location, a stack of return nodes, etc.), as well as information about the `Pointer`'s location
+/// within the original source string passed into the `WordFilter`.
+///
+/// In order to progress the `Pointer` forward, the `step()` method is provided, which allows the
+/// user to step the `Pointer` through each character in a string.
 #[derive(Clone)]
 pub struct Pointer<'a> {
+    /// The current node that is being pointed to.
     pub current_node: &'a Node<'a>,
+    /// A stack of return nodes (indicating the `Pointer`'s context within subgraphs).
     pub return_nodes: Vec<&'a Node<'a>>,
+    /// This `Pointer`'s current status.
     pub status: PointerStatus<'a>,
 
+    /// The start index within the original source string.
     pub start: usize,
+    /// The length which this `Pointer` has traveled.
     pub len: usize,
+    /// The length where the last `Match` or `Exception` node was found.
     pub found_len: Option<usize>,
 }
 
 impl<'a> Pointer<'a> {
+    /// Creates a new `Pointer` with the provided attributes.
+    /// 
+    /// This also sets `status` to `PointerStatus::None` and `found_len` to `None`.
     pub fn new(
         current_node: &'a Node<'a>,
         return_nodes: Vec<&'a Node<'a>>,
@@ -35,6 +58,13 @@ impl<'a> Pointer<'a> {
         }
     }
 
+    /// Processes a return node.
+    ///
+    /// This evaluation is very conservative. If the node is not actually a return node, the node
+    /// is itself returned.
+    ///
+    /// If the node *is* a return node, and the `return_nodes` stack is empty, then `None` is 
+    /// returned. Otherwise, the new node where the `Pointer` should be located is returned.
     fn evaluate_return_node(&mut self, node: &'a Node<'a>) -> Option<&'a Node<'a>> {
         match node.node_type {
             NodeType::Standard => Some(node),
@@ -58,6 +88,10 @@ impl<'a> Pointer<'a> {
         }
     }
 
+    /// Step the `Pointer` along the character `c`.
+    ///
+    /// If the `Pointer` has reached a dead-end, this method returns `false`. Otherwise, it returns
+    /// `true` to indicate the `Pointer` is still active in the `WordFilter` graph.
     pub fn step(&mut self, c: &char) -> bool {
         self.current_node = match self.current_node.children.get(c) {
             Some(node) => match node.node_type {

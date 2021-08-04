9aef46fb6c78b7b125af45859691f5ae462005f0
Anders429 <anders.stefan.evensen@gmail.com>
2021-08-04 07:49:57 -0700

    Define state Flags bitflags.

--- src/pda.rs
+++ src/pda.rs
@@ -12,6 +12,7 @@
 #![doc(hidden)]
 
 use alloc::{vec, vec::Vec};
+use bitflags::bitflags;
 use by_address::ByAddress;
 use core::{
     ops::{Bound, RangeBounds},
@@ -19,6 +20,31 @@ use core::{
 };
 use debug_unreachable::debug_unreachable;
 use hashbrown::HashSet;
+
+bitflags! {
+    /// Bitflags that define attributes on a [`State`].
+    ///
+    /// These flags define specific attributes on a `State`. Multiple flags may be set at the same
+    /// time.
+    pub struct Flags: u8 {
+        /// The state is matching state, matching a word.
+        ///
+        /// If this flag is set, a word should be stored within the state as well.
+        const WORD = 0b0000_0001;
+        /// The state is a matching state, matching an exception.
+        const EXCEPTION = 0b0000_0010;
+        /// This is a separator state, existing within a separator subroutine.
+        const SEPARATOR = 0b0000_0100;
+        /// [`InstantaneousDescription`]s can return from this state.
+        const RETURN = 0b0000_1000;
+        /// This state can be repeated to.
+        const INTO_REPETITION = 0b0001_0000;
+        /// This state can process a repetition on the stack.
+        const TAKE_REPETITION = 0b0010_0000;
+        /// This state can enter a separator subroutine.
+        const INTO_SEPARATOR = 0b0100_0000;
+    }
+}
 
 /// The different types of states.
 ///
